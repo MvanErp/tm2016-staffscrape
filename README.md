8c9042f266014d928b0982fcad09e7e2868d4593
Marieke van Erp <marieke.van.erp@vu.nl>
2016-02-05 12:31:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tm2016-staffscrape
---
+++ TM2016-StaffScrape.py
@@ -0,0 +1,49 @@
+#!-*- coding: utf-8 -*-
+
+from urllib2 import Request, urlopen, URLError
+from bs4 import BeautifulSoup
+import time
+import sys
+import os
+import re 
+
+fname = sys.argv[1] 
+
+# Open the file with the urls and read it in 
+with open(fname) as f:
+    content = f.readlines()
+f.close() 
+    
+# Loop through the urls and gather the follow up urls 
+for line in content:
+    line = line.rstrip()
+    elements = line.split('\t')
+    url = elements[1]
+    html = urlopen(url).read()
+    souped = BeautifulSoup(html, 'html.parser')
+    links_to_follow = {}
+    # If this is the first website in a particular research group, create a dir for it
+    groupname = elements[0]
+    if not os.path.exists(groupname):
+        os.makedirs(groupname)
+    # Create a directory inside the dir for this researcher's info
+    url_stripped = re.sub(r'\W+', '', url)
+    persondir = groupname + '/' + url_stripped
+    if not os.path.exists(persondir):
+        os.makedirs(persondir)
+    # Find all the links in the page that you want to follow i.e. you only want to follow links on the same domain 
+    for link in souped.find_all('a'):
+        result = str(link.get('href'))
+        if url in result:
+            links_to_follow[result] = 1
+    # Now you've gathered the links you want to pull information from, so then you can start!         
+    for link in links_to_follow:
+        print "Now requesting: " + link 
+        page = urlopen(link).read()
+        pagename = re.sub(r'\W+', '', link)
+        filename = persondir + '/' + pagename + '.html'
+        f = open(filename, 'w')
+        f.write(page)
+        f.close(page)
+        print "Slight delay of 5 seconds to not piss off any server"
+        time.sleep(5) 
